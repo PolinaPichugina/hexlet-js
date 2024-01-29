76465b73849d99ff2e55c563d3a5714b07262c09
Polina Pichugina <p.cipileva@mail.ru>
2024-01-29 18:04:20 +0300

    missionUpdate

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
 import _ from "lodash";
 import strangeMission from "./src/index.js"
-console.log(strangeMission);
+strangeMission();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export default (strangeMission) => {
-return "Hello, Hexlet!";
+console.log("Hello, Hexlet!");
 }
